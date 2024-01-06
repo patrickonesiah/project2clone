7922daa791c12adbb735abc0246f035b35bc60bf
patrickonesiah <patrickonesiah@gmail.com>
2024-01-07 01:39:47 +1100

    Prep display story page

--- src/Components/DisplayStory.js
+++ src/Components/DisplayStory.js
@@ -3,26 +3,41 @@ import { useParams } from "react-router-dom"
 import { useHistory } from "react-router-dom";
 function DisplayStory() {
     const [project, setProject] = useState(null);
-    const {id} = useParams()
+    const { id } = useParams()
     const history = useHistory();
     useEffect(() => {
         fetch(`http://localhost:3001/stories/${id}`)
             .then(r => r.json())
             .then(data => setProject(data))
     }, [id])
-    
-    if (!project) return <h2>Loading...</h2>
-    
-    const { storyTitle } = project
 
-    function handleNewStoryTitle(){
+    if (!project) return <h2>Loading...</h2>
+
+    const { storyTitle, storyDescription, childFirstName, childLastName, authorFirstName, authorLastName, relationship, writtenDate, numberOfLikes, birthOrder, whoHasSeen, storyMainImage } = project;
+    function handleNewStoryTitle() {
         history.push(`/EditStory/${id}`)
     }
-    
+
     return (
-        <div>
-            <h1>{storyTitle}</h1>
-            <button onClick={handleNewStoryTitle}>Edit</button>
+        <div className="App-stories">
+            <div className="App-story">
+                <h1>{storyTitle}</h1>
+
+                <div className="Display-story-top">
+                    <h2>{childFirstName} {childLastName} - {birthOrder} child</h2>
+                    <img className="App-story-image" src={storyMainImage} alt="{description}" />
+                    <p>{storyDescription}</p>
+                </div>
+
+                <div className="App-story-bottom">
+                    <span className="authorName">Shared by {authorFirstName} {authorLastName}</span>
+                    <span className="writtenDate">{writtenDate}</span>
+                    <span className="numberOfLikes">{numberOfLikes}</span>
+                    {/* <button className="deleteButton" onClick={handleDelete}>🗑</button> */}
+                </div>
+
+                <button onClick={handleNewStoryTitle}>Edit</button>
+            </div>
         </div>
     );
 }
--- src/Components/UserProfile.js
+++ src/Components/UserProfile.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function UserProfile() {
+
+  const [newName, setNewName] = useState("Phil Dunphy")
+  const [isEditable, setEditable] = useState(false)
+
+  function handleEditable() {
+    setEditable(!isEditable)
+  }
+
+  function handleChangeName(e) {
+    setNewName(e.target.value)
+  }
   return (
     <div className="App-user-profile">
-        <div className="App-user-profile-left">
-            <img className="App-user-profile-img" src="./images/userProfileImage.png" alt="{description}" />
-        </div>
-        <div className="App-user-profile-middle">
-            <h2>Phil Dunphy</h2>
-            <button>Edit</button>
-        </div>
-        <div className="App-user-profile-right">
+      <div className="App-user-profile-left">
+        <img className="App-user-profile-img" src="./images/userProfileImage.png" alt="{description}" />
+      </div>
+      <div className="App-user-profile-middle">
+        {
+          isEditable ?
+            (<input className="App-user-name" name={newName} value={newName} onChange={handleChangeName} />)
+            : (<h2>{newName}</h2>)
+        }
+      </div>
+      <div className="App-user-editButton">
+        <button  onClick={handleEditable}>{isEditable ? "Save" : "Edit"}</button>
+      </div>
+      <div className="App-user-profile-right">
 
-        </div>
+      </div>
     </div>
   );
 }
